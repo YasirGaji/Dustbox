95b031127b640971ab8c5aafe1341181b2c7de53
yasir <hellopleasantcodes@gmail.com>
2022-01-13 18:49:21 +0100

    dynamically rendered individual date properties

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>January 12th 2022</div>
+      <div>
+        <div>{month}</div>
+
+        <div>{year}</div>
+
+        <div>{day}</div>
+      </div>
 
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">₦2000</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₦{props.amount}</div>
       </div>
       
     </div>
