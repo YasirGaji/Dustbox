e316120ce0cab016cf38aa3e87d2bbd61a6461a1
yasir <hellopleasantcodes@gmail.com>
2022-01-18 12:28:58 +0100

    added a form component to the UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 
 const App = () => {
@@ -26,7 +28,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
