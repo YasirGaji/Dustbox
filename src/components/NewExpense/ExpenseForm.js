59aacf9e552e2b7c7ac8ab27644a46fddfd38e9c
yasir <hellopleasantcodes@gmail.com>
2022-01-18 14:59:03 +0100

    changed single individual state management to single multiple

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 export default function ExpenseForm() {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  });
+
+  const titleChangeHandler = (e) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredTitle: e.target.value
+      }
+    })
+  }
+
+  const amountChangeHandler = (e) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredAmount: e.target.value
+      }
+    })
+  }
+
+  const dateChangeHandler = (e) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredDate: e.target.value
+      }
+    })
+  }
+  
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' steps='0.01' />
+          <input type='number' min='0.01' steps='0.01' onChange={amountChangeHandler} />
         </div>
 
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2022-01-01' max='2030-12-31' />
+          <input type='date' min='2022-01-01' max='2030-12-31' onChange={dateChangeHandler} />
         </div>
       </div>
 
