7415354acca1a353423fd2bf473df457de080233
yasir <hellopleasantcodes@gmail.com>
2022-01-21 09:39:48 +0100

    took out button from the expense item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-
-  const clickHandler = () => {
-    setTitle('Title Changed');
-  }
-
   return (
     <Card className="expense-item">
       <div>
@@ -18,11 +11,9 @@ export default function ExpenseItem(props) {
       </div>
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">₦{props.amount}</div>
       </div>
-
-      <button onClick={clickHandler}>Change Title</button>  
     </Card>
   )
 }
