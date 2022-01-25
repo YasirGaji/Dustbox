24031626ae21987ed3aff49399402499a6ab119f
yasir <hellopleasantcodes@gmail.com>
2022-01-25 09:12:38 +0100

    made semantic change

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,17 @@ import Card from '../UI/Card';
 
 export default function ExpenseItem(props) {
   return (
-    <Card className="expense-item">
-      <div>
-        <ExpenseDate date={props.date} />
-      </div>
+    <li>
+      <Card className="expense-item">
+        <div>
+          <ExpenseDate date={props.date} />
+        </div>
 
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">₦{props.amount}</div>
-      </div>
-    </Card>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">₦{props.amount}</div>
+        </div>
+      </Card>
+    </li>
   )
 }
