908ce03ec8a95854f7b99dcbe5ad684efe57b7b6
yasir <hellopleasantcodes@gmail.com>
2022-01-25 08:51:53 +0100

    restructured conditional statement to lean snippet

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,6 +15,18 @@ export default function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => (
+      <ExpenseItem 
+        key = {expense.id}
+        title = {expense.title}
+        amount = {expense.amount}
+        date = {expense.date }
+    />))
+  }
+
   return (
     <div>
       <Card className='expenses'>
@@ -25,17 +37,7 @@ export default function Expenses(props) {
           />
         </div>
 
-        {filteredExpenses.length === 0 ? (<p>No expenses found.</p>) : (
-          filteredExpenses.map(expense => (
-          <ExpenseItem 
-            key = {expense.id}
-            title = {expense.title}
-            amount = {expense.amount}
-            date = {expense.date }
-        />))
-        )}
-
-        
+        {expensesContent}
       </Card>
     </div>
   )
