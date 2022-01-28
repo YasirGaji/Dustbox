deb166571e045d0653b0a93cf017d73324b8e1ea
yasir <hellopleasantcodes@gmail.com>
2022-01-28 21:30:14 +0100

    dynamically rendered clasnames and installed styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
 
-import './Button.css';
+const Button = styled.button`
+  
+`;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+{// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
+}
 
 export default Button;
