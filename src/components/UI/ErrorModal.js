c8f562c044bfab8d2eda1b69a981ec2cc67c1543
yasir <hellopleasantcodes@gmail.com>
2022-02-08 07:57:31 +0100

    introduced to portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import style from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 import Wrapper from '../Helpers/Wrapper';
 
+const Backdrop = props => {
+  return (
+    <div className={style.backdrop} onClick={props.onConfirm} />
+  );
+}
+
+const ModalOverlay = props => {
+  return (
+    <Card className={style.modal}>
+      <header className={style.header}> 
+        <h2>{props.title}</h2>
+      </header>
+
+      <div className={style.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={style.actions}>
+        <Button onClick={props.onConfirm}>Close</Button>
+      </footer>
+    </Card>
+  )
+}
+
 function ErrorModal(props) {
   return (
     <Wrapper>
-      <div className={style.backdrop} onClick={props.onConfirm} />
-      <Card className={style.modal}>
-        <header className={style.header}> 
-          <h2>{props.title}</h2>
-        </header>
+     {ReactDOM.createPortal(
+       <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+     )}
 
-        <div className={style.content}>
-          <p>{props.message}</p>
-        </div>
-
-        <footer className={style.actions}>
-          <Button onClick={props.onConfirm}>Close</Button>
-        </footer>
-      </Card>
+     {ReactDOM.createPortal(
+       <ModalOverlay 
+          onConfirm={props.onConfirm} 
+          title={props.title}
+          message={props.message}
+        />,
+        document.getElementById('overlay-root')
+     )}
     </Wrapper>
   );
 }
