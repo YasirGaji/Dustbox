f7420afd6b929c5146abf7d1e6a208f4795e9b6d
yasir <hellopleasantcodes@gmail.com>
2022-02-10 16:05:32 +0100

    partially understood useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,6 +12,14 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log('EFFECT RUNNING');
+
+    return () => {
+      console.log('EFFECT CLEANUP')
+    }
+  }, [enteredPassword]);
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('checking');
       setFormIsValid(
