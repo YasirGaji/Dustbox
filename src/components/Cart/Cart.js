2bb390ba1eb21f5b2d32d2fdc2af52a1e03c3205
yasir <hellopleasantcodes@gmail.com>
2022-03-02 12:23:43 +0100

    refactored the cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Modal from '../UI/Modal';
 import style from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 export default function Cart(props) {
   const cartCtx = useContext(CartContext);
@@ -9,10 +10,21 @@ export default function Cart(props) {
   const totalAmount = `₦${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {}
+
   const cartItems = <ul className={style['cart-items']}>
     {cartCtx.items.map((item) => 
-      <li>{item.name}</li>)
-    }
+      <CartItem
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        price={item.price}
+        amount={item.amount}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        onAdd={cartItemAddHandler.bind(null, item)}
+      />
+    )}
   </ul>;
 
   return (
