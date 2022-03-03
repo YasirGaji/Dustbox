70c081f8f8102ad959678d53316e5dff75d3580a
yasir <hellopleasantcodes@gmail.com>
2022-03-03 17:33:45 +0100

    proven that for every state changed the entire app would be re-rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
-
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
+  console.log('runnning');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevshowParagraph => !prevshowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {showParagraph && <p>This is a simple React app.</p>}
+
+      <Button onClick={toggleParagraphHandler}>
+        Toggle paragraph
+      </Button>
     </div>
   );
 }
