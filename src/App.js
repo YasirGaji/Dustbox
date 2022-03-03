b824c6cc5e8b3e04d2dc7ee67416b206c7bd1ef1
yasir <hellopleasantcodes@gmail.com>
2022-03-03 17:24:14 +0100

    conditioned the paragraph using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is a simple React app.</p>}
     </div>
   );
 }
