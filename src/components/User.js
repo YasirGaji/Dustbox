ec1b1bcd8778a3e93635dd7178acef6d04b207d6
yasir <hellopleasantcodes@gmail.com>
2022-03-16 17:34:36 +0100

    worked with component did mount

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+
+class User extends Component {
+  render() {
+    return (
+      <div className={classes.user}>
+        {this.props.name}
+      </div>
+    );
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
