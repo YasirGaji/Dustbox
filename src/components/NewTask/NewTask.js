d5822b5cc898d76eadc0d46740fafb8067b9ff49
yasir <hellopleasantcodes@gmail.com>
2022-03-19 18:37:37 +0100

    added right firebase links

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
+        'https://react-http-4a5c5-default-rtdb.firebaseio.com/tasks.json',
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
