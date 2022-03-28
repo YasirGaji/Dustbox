5709154755133a935f2c0b7c1812e1c51288d7d9
yasir <hellopleasantcodes@gmail.com>
2022-03-28 14:52:05 +0100

    integerated the basic form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,44 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    } else {
+      setEnteredNameIsValid(true);
+      // props.onAdd(enteredName);
+      // setEnteredName('');
+
+      const enteredValue = nameInputRef.current.value;
+      console.log(enteredValue);
+    }
+  }
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+          ref={nameInputRef}
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler} 
+        />
       </div>
+      {!enteredNameIsValid && <p className='error-text'>Name bar must not be empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
