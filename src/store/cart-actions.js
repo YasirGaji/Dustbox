0cfe8a64dee7c5ea1eec1ebd7c64edb815442ff8
yasir <hellopleasantcodes@gmail.com>
2022-04-26 07:03:50 +0100

    completely added the http logic using redux

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -21,7 +21,10 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity,
+      }));
     } catch(error) {
       dispatch(
         uiActions.showNotification({
