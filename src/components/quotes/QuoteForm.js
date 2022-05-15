7b296d422aa9ef3ebb31c6f4fe0317d182445e1f
yasir <hellopleasantcodes@gmail.com>
2022-05-15 13:09:46 +0100

    prevented unwanted routing using prompt

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,12 @@
-import { useRef } from 'react';
-
+import React, { useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +21,42 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  }
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  }
+
+  return (
+    <>
+      <Prompt 
+        when={isEntering} 
+        message={(location) => 'Are you sure you want to leave? All your changes will be lost.'} 
+      />
+      <Card>
+        <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className='btn'>Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
 
 function NewQuotes() {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     console.log(quoteData);
+
+    history.push('/quotes');
   }
 
   return (
