b0ccdd5090c7b4c10dbe73fff75b99c4d91b5fdd
yasir <hellopleasantcodes@gmail.com>
2022-05-17 07:28:59 +0100

    Done with the project but having issues with the nested routing for the comments section on the view fullscreen page

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,49 @@
-import { useState } from 'react';
-
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/hooks/use-http';
+import { getAllComments } from '../../lib/lib/api';
+import LoadingSpinner from '../../components/UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback (() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = <div className='centered'>
+      <LoadingSpinner />
+    </div>;
+  }
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <div className='centered'>
+      <p>No comments yet.</p>
+    </div>;
+  }
   
   return (
     <section className={classes.comments}>
@@ -18,8 +53,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+      {comments} 
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,20 +1,41 @@
-import { useRef } from 'react';
-
+import { useRef, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+import useHttp from '../../hooks/hooks/use-http';
+import { addComment } from '../../lib/lib/api';
 import classes from './NewCommentForm.module.css';
+import LoadingSpinner from '../../components/UI/LoadingSpinner';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  // const params = useParams();
+
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment])
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
+
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: {text: enteredText}, quoteId: props.quoteId }); // send comment to server
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
