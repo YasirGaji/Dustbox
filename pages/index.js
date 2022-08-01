31b8b731a6caf4c4e09af8847cc5ae7a2239ee77
Yasir Gaji <gajiyasir@gmail.com>
2022-08-01 03:13:00 +0100

    added base page

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/7e/Thomas_Wolfe%27s_Home.jpg',
+    address: '123 Main St',
+    description: 'This is the first meetup',
+  },
+
+  {
+    id: 'm2',
+    title: 'Second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/7e/Thomas_Wolfe%27s_Home.jpg',
+    address: '123 Main St',
+    description: 'This is the second meetup',
+  },
+
+  {
+    id: 'm3',
+    title: 'Third meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/7e/Thomas_Wolfe%27s_Home.jpg',
+    address: '123 Main St',
+    description: 'This is the third meetup',
+  },
+
+  {
+    id: 'm4',
+    title: 'first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/7e/Thomas_Wolfe%27s_Home.jpg',
+    address: '123 Main St',
+    description: 'This is the fourth meetup',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
