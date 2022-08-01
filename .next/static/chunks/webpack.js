3bc99e36ee829ed1c24d95be603a766548d8a2b5
Yasir Gaji <gajiyasir@gmail.com>
2022-08-01 03:59:41 +0100

    learnt about the useRouter hook in next and how to use it to push dynamic pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b67c1dfcb43075b"; }
+/******/ 		__webpack_require__.h = function() { return "2753a35c66f167f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
